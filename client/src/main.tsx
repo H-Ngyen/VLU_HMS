57555f55b34fa71ac1620ff841da2062402c1209
khaizr0 <54585187+khaizr0@users.noreply.github.com>
2026-04-06 11:41:08 +0700

    Client: Auth0 small fix

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -14,6 +14,8 @@ createRoot(document.getElementById("root")!).render(
       clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
       authorizationParams={{
         redirect_uri: window.location.origin,
+        audience: import.meta.env.VITE_AUTH0_AUDIENCE,
+        scope: "openid profile email",
       }}
       cacheLocation="localstorage"
     >
