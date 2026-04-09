abd23a2c7970acb3f8f0b85067345d1eef38ded8
H-Ngyen <hoangnguyennn0210@gmail.com>
2026-04-09 17:32:20 +0700

    fix(api): super fix user context

--- API/Domain/Constants/AppClaimTypes.cs
+++ API/Domain/Constants/AppClaimTypes.cs
@@ -3,7 +3,7 @@ namespace Domain.Constants;
 public static class AppClaimTypes 
 {
     public const string Name = "name";
-    public const string EmailVerified = "name";
+    public const string EmailVerified = "email_verified";
     public const string UpdatedAt = "updated_at";
     public const string Picture = "picture";
 }
